73fdfac275c51ed53e45f18412a90e799732490a
delan-eoin <delaneyeoin13@gmail.com>
2022-11-07 09:49:56 +0000

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,3 +1,4 @@
+create database group_project;
 use group_project;
 /*Forget password*/
 
@@ -17,37 +18,41 @@ create table create_account
 /*Log into account*/
 create table login
 (
-    login_id auto_increment primary key,
-	unique_id integer foreign key,
+    login_id integer primary key auto_increment,
+	unique_id integer,
     email varchar(50),
-    password varchar(40)
+    password varchar(40),
+    constraint login_fk foreign key (unique_id) references create_account (unique_id)
 );
 /*Dashboard*/
 create table dashboard
 (
     dashboard_id integer auto_increment primary key,
-    login_id integer foreign key,
+    login_id integer,
 	power_use varchar(20),
     water_use varchar(20),
-    temperature varchar(20)
+    temperature varchar(20),
+    constraint dashboard_fk foreign key (login_id) references login (login_id)
 );
 /*Prediction*/
 create table prediction
 (
 	prediction_id integer auto_increment primary key,
-    dashboard_id integer foreign key,
+    dashboard_id integer,
     date_entered date,
     power_use varchar(20),
     water_use varchar(20),
-    temperature varchar(20)
+    temperature varchar(20),
+    constraint prediction_fk foreign key (dashboard_id) references dashboard (dashboard_id)
 );
 /*Graph Page*/
 /*Prepared statement for the actual graph?*/
 create table graph_page
 (
     graph_page_id integer auto_increment primary key,
-    dashboard_id integer foreign key,
+    dashboard_id integer,
 	to_date date,
-    from_date date
+    from_date date,
+    constraint graph_page_fk foreign key (dashboard_id) references prediction (dashboard_id)
 );
 
