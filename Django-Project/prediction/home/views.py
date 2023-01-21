f08c43db42bfd8e3fd1210f1bb47535d0020bb87
delan-eoin <delaneyeoin13@gmail.com>
2023-01-21 19:29:41 +0000

    Login function
    User can login to application in django

--- Django-Project/prediction/home/views.py
+++ Django-Project/prediction/home/views.py
@@ -29,7 +29,23 @@ def dashboard(request):
     #return HttpResponse("this is the dashboard")
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}.")
+                return redirect("main:homepage")
+            else:
+                messages.error(request, "Invalid username or password.")
+        else:
+            messages.error(request, "Invalid username or password.")
+    form = AuthenticationForm()
+    return render(request=request, template_name="login.html", context={"login_form":form})
+    #return render(request, 'login.html')
     #return HttpResponse("this is the login page")
 
 def prediction(request):
