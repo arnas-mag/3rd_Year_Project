8c2d3681b47bf9e8cf43d6ca84cd89d541cfdb73
Vinicius Parzanini <115189769+FishBonesHybrid@users.noreply.github.com>
2023-02-06 09:04:17 +0000

    Add files via upload
    most updated version, make sure to change the directory of the csv files.

---
+++ Django-Project/prediction/prediction/home/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+# Create user form 
+
+class NewUserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    #first_name = forms.CharField(widget=forms.TextInput(
+     #   attrs={
+      #      'type':'first_name',
+       #     'placeholder':('First Name')
+        #}
+    #))
+    
+    #last_name = forms.CharField(widget=forms.TextInput(
+    #    attrs={
+    #        'type':'last_name',
+    #        'placeholder':('Last Name')
+    #    }
+    #))
+    
+    #email = forms.CharField(widget=forms.TextInput(
+    #    attrs={
+    #        'type':'email',
+    #        'placeholder':('Email')
+    #    }
+    #))
+    
+    #password1 = forms.CharField(max_length=16,widget=forms.TextInput(
+    #    attrs={
+    #        #'type':'first_name',
+    #        'placeholder':('Password')
+    #    }
+    #))
+    
+    #password2 = forms.CharField(max_length=16,widget=forms.TextInput(
+    #    attrs={
+    #        #'type':'first_name',
+    #        'placeholder':('Repeat Password')
+    #    }
+    #))
+    
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name','email', 'password1', 'password2')
+        
+    def save(self, commit=True):
+        user = super(NewUserForm, self).save(commit=False)
+        user.email = self.cleaned_data['email']
+        if commit:
+            user.save()
+        return user
+
+class data(forms.Form):
+    avgTemp = forms.DecimalField(max_digits=10, decimal_places=2, label='Temperature')
+    rainfall = forms.DecimalField(max_digits=10, decimal_places=2)
+    # file = forms.FileField()
